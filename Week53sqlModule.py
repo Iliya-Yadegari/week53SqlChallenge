92ea7f17b86766b0a52653b6ecec5e29da8d2d22
Iliya-Yadegari <naruto3@email.com>
2021-06-06 20:02:41 +0100

    Create Week53sqlModule.py

---
+++ Week53sqlModule.py
@@ -0,0 +1,22 @@
+import mysql.connector as msc
+
+mydb = msc.connect(
+            host = 'localhost',
+            user = 'root',
+            password = '',
+            database = 'week53'
+            )
+        
+mycursor = mydb.cursor()
+
+def write_fun(name_e,place_e,age_e):
+    
+    sqlCommand = 'INSERT INTO results (name,placeOfBirth,age) VALUES (%s,%s,%s)'
+    values = (name_e,place_e,age_e)
+    mycursor.execute(sqlCommand,values)
+    mydb.commit()
+
+    mycursor.execute('SELECT age FROM results')
+
+    result = mycursor.fetchall()
+    print(result)
